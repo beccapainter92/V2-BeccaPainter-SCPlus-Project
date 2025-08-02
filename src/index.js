2425f54e0a07b4c5d44cc363c7cb4653ee0ff9cb
beccapainter92 <beccapainter92@gmail.com>
2025-08-02 19:23:38 +1000

    dynamic date and forecast update

--- src/index.js
+++ src/index.js
@@ -53,6 +53,13 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "8t3bb4640173o53eaba9b59f80d9e4f2";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -63,25 +70,29 @@ function getForecast(city) {
 function displayForecast(response) {
   console.log(response.data);
 
-  let days = ["Tues", "Wed", "Thurs", "Fri", "Sat"];
   let forecastHTML = "";
 
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `
     <div class="weather-forecast-day">
-            <div class="weather-forecast-date">${day}</div>
-            <div class="weather-forecast-icon">☀️</div>
-            <div class="weather-forecast-temperatures">
+      <div class="weather-forecast-date"> ${formatDay(day.time)}   </div>
+      <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+        <div class="weather-forecast-temperatures">
               <div class="weather-forecast-temperature">
-                <strong> 15°C</strong>
+                <strong>${Math.round(day.temperature.maximum)}°C</strong>
               </div>
-              <div class="weather-forecast-temperature">7°C</div>
+              <div class="weather-forecast-temperature">${Math.round(
+                day.temperature.minimum
+              )}°C</div>
             </div>
           </div>
           `;
+    }
   });
+
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHTML;
 }
